e94e5791cd21f929813633e0380ae2cbf6ff707e
Jasurbek <jasurjoon777gmail.com>
2023-04-13 12:46:49 +0500

    Project Started By Jasur

---
+++ routes/web.php
@@ -0,0 +1,56 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+// Nav Routes Start
+
+Route::get('/', function () {
+    return view('index');
+})->name('index');
+
+Route::get('/about', function () {
+    return view('about');
+})->name('about');
+
+Route::get('/products', function () {
+    return view('products');
+})->name('products');
+
+Route::get('/products', function () {
+    return view('products');
+})->name('products');
+
+Route::get('/singleProduct', function () {
+    return view('singleProduct');
+})->name('singleProduct');
+
+Route::get('/contact', function () {
+    return view('contact');
+})->name('contact');
+
+// Nav Routes End
+
+Route::prefix('admin')->middleware(['auth'])->name('admin.')->group(function () {
+    Route::get('/dashboard', function () {
+        return view('admin.dashboard');
+    })->name('dashboard');
+});
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__ . '/auth.php';
